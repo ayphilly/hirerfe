d06d93de973472d73e9a0ac6120a9579fb1c2bb2
jbrit <pro.ajibolaojo@gmail.com>
2021-05-22 00:34:26 +0100

    sass modules and first form step added

--- src/App.js
+++ src/App.js
@@ -1,72 +1,128 @@
-import logo from './logo.svg';
-import './App.scss';
-import {Route, Switch } from 'react-router-dom';
-import Homepage from "./talent/pages/homepage/talent"
-import {Dashboardhome} from "./talent/pages/dashboard/homepage/homepage"
-import {Myjobs} from "./talent/pages/dashboard/myjobs/myjobs"
-import {Profile} from "./talent/pages/dashboard/profile/profile"
-import {Searchjob} from "./talent/pages/searchjob/searchjob"
-import {Searchtalent} from "./hirer/pages/searchtalents/searchtalent"
-import Hirer from "./hirer/pages/homepage/hirer"
-import {Createjob} from "./hirer/pages/postjob/createjob"
-import Footer from "./generals/footer"
-import {Signin} from "./generals/signin/signin"
-import Talentnav  from "./talent/components/navbar"
-import Dashboardnav  from "./talent/components/dashboardnavbar/dashboardnav"
-import Hirernav from "./hirer/components/navbar/navbar"
-import Createaccount from "./generals/createaccount/createaccount"
-import {closePage} from "./helper"
+import logo from "./logo.svg";
+import "./App.scss";
+import { Route, Switch } from "react-router-dom";
+import Homepage from "./talent/pages/homepage/talent";
+import { Dashboardhome } from "./talent/pages/dashboard/homepage/homepage";
+import { Myjobs } from "./talent/pages/dashboard/myjobs/myjobs";
+import { Profile } from "./talent/pages/dashboard/profile/profile";
+import { Searchjob } from "./talent/pages/searchjob/searchjob";
+import { Searchtalent } from "./hirer/pages/searchtalents/searchtalent";
+import Hirer from "./hirer/pages/homepage/hirer";
+import { Createjob } from "./hirer/pages/postjob/createjob";
+import Footer from "./generals/footer";
+import { Signin } from "./generals/signin/signin";
+import Talentnav from "./talent/components/navbar";
+import Dashboardnav from "./talent/components/dashboardnavbar/dashboardnav";
+import Hirernav from "./hirer/components/navbar/navbar";
+import Createaccount from "./generals/createaccount/createaccount";
+import { closePage } from "./helper";
 import { gsap, ScrollTrigger, Draggable, MotionPathPlugin } from "gsap/all";
+import PostJob from "./hirer/pages/PostJob";
 
-const TalentWithNavbar = ({exact, path, component:Component, ...rest}) => {
-  return <Route exact={exact} path={path} {...rest} render={(routeProps) => {
-     return <><Talentnav {...routeProps}/><Component {...routeProps}/> </>
-  }}
-  />
-}
+const TalentWithNavbar = ({ exact, path, component: Component, ...rest }) => {
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      {...rest}
+      render={(routeProps) => {
+        return (
+          <>
+            <Talentnav {...routeProps} />
+            <Component {...routeProps} />{" "}
+          </>
+        );
+      }}
+    />
+  );
+};
 
-const HirerWithNavbar = ({exact, path, component:Component, ...rest}) => {
-  return <Route exact={exact} path={path} {...rest} render={(routeProps) => {
-     return <><Hirernav {...routeProps}/><Component {...routeProps}/> </>
-  }}
-  />
-}
+const HirerWithNavbar = ({ exact, path, component: Component, ...rest }) => {
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      {...rest}
+      render={(routeProps) => {
+        return (
+          <>
+            <Hirernav {...routeProps} />
+            <Component {...routeProps} />{" "}
+          </>
+        );
+      }}
+    />
+  );
+};
 
-const DashboardWithNavbar = ({exact, path, component:Component, ...rest}) => {
-  return <Route exact={exact} path={path} {...rest} render={(routeProps) => {
-     return <><Dashboardnav {...routeProps}/><Component {...routeProps}/> </>
-  }}
-  />
-}
+const DashboardWithNavbar = ({
+  exact,
+  path,
+  component: Component,
+  ...rest
+}) => {
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      {...rest}
+      render={(routeProps) => {
+        return (
+          <>
+            <Dashboardnav {...routeProps} />
+            <Component {...routeProps} />{" "}
+          </>
+        );
+      }}
+    />
+  );
+};
 
 function App() {
-  
-  
-
   return (
     <div className="App">
       <Switch>
         {/* <Route exact path="/" render = { (props)=> <Homepage {...props} closePage={closePage} openPage ={openPage}/> }   />
         <Route path="/searchjob" component={Searchjob} />  */}
-        <TalentWithNavbar exact path='/' component={Homepage} />
-        <TalentWithNavbar exact path='/talent/searchjob' component={Searchjob} />
+        <TalentWithNavbar exact path="/" component={Homepage} />
+        <TalentWithNavbar
+          exact
+          path="/talent/searchjob"
+          component={Searchjob}
+        />
 
         {/* <Route path="/hirer" render = { (props)=> <Hirer {...props} closePage={closePage} openPage ={openPage}/> } /> 
         <Route path="/postjob" component={Createjob} /> 
         <Route path="/searchtalent" component={Searchtalent} />  */}
 
-        <HirerWithNavbar exact path='/hirer' component={Hirer} />
-        <HirerWithNavbar exact path="/hirer/postjob" component={Createjob} />
-        <HirerWithNavbar exact path="/hirer/searchtalent" component={Searchtalent} />
+        <HirerWithNavbar exact path="/hirer" component={Hirer} />
+        <HirerWithNavbar exact path="/hirer/postjob" component={PostJob} />
+        <HirerWithNavbar
+          exact
+          path="/hirer/searchtalent"
+          component={Searchtalent}
+        />
 
-        <Route path="/signin" component={Signin} /> 
-        
+        <Route path="/signin" component={Signin} />
+
         {/* Dashboard Talent */}
 
-        <DashboardWithNavbar exact path="/dashboard" component={Dashboardhome}/>
-        <DashboardWithNavbar exact path="/dashboard/myjobs" component={Myjobs}/>
-        <DashboardWithNavbar exact path="/dashboard/profile" component={Profile}/>
-        
+        <DashboardWithNavbar
+          exact
+          path="/dashboard"
+          component={Dashboardhome}
+        />
+        <DashboardWithNavbar
+          exact
+          path="/dashboard/myjobs"
+          component={Myjobs}
+        />
+        <DashboardWithNavbar
+          exact
+          path="/dashboard/profile"
+          component={Profile}
+        />
+
         {/* <Route component={Error} /> */}
       </Switch>
       <div className="overlay hidden"></div>
--- src/hirer/pages/PostJob.js
+++ src/hirer/pages/PostJob.js
@@ -6,7 +6,34 @@ const PostJob = () => {
   const nextStep = () => setStep(step + 1);
   return (
     <>
-      <div className="postajob-container"></div>
+      <div
+        style={{ width: "100%", padding: "100px 0" }}
+        className="d-flex flex-column align-items-center"
+      >
+        <div
+          style={{
+            backgroundColor: "#fff",
+            width: "90%",
+            maxWidth: "500px",
+            boxShadow: "0 0 20px rgba(0,0,0,0.1)",
+          }}
+          className="plain-card d-flex flex-column "
+        >
+          <div className="f-24 fw-700">Step 1 of 4 &bull; Job Title</div>
+          <p className="mb-8">
+            Enter your details to join The #1 Mastermind On Global Communities -
+            No Catch - Pay Once, get twelve months full access.
+          </p>
+          <label className="mb-3" htmlFor="jobTitle">
+            Job Title
+            <span style={{ color: "red" }}>*</span>
+          </label>
+          <input className="mb-8 classic-input" type="text" />
+          <button onClick={nextStep} className="btn btn-primary w-100">
+            Next
+          </button>
+        </div>
+      </div>
     </>
   );
 };
