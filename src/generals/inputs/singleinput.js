0f6b8fc516ed3e0b6b83a237816a0da02b8dcb54
waleofblanco <mac@WaleofBlancos-MacBook-Pro.local>
2021-07-22 14:59:33 +0100

    edited singleinput component

--- src/generals/inputs/singleinput.js
+++ src/generals/inputs/singleinput.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import "./singleinput.scss"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
 import { useState} from "react"
-=======
-import "./singleinput.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-import { useState, useEffect } from "react";
->>>>>>> 8007ad382bd7a9f787874a8bad72b6ebfd55e456
 
 const Singleinput = (props) => {
   const [passwordShown, setPasswordShown] = useState(false);
@@ -64,29 +57,9 @@ const Singleinput = (props) => {
           />
         )}
       </div>
+      <p  style={{color:'red', height:5, width:100+'%'}} > {props.error ? props.error : " "}</p>
 
-<<<<<<< HEAD
-    return (
-        <div className="single-form-input">
-            <label className="sing-form-label"> {props.label} </label>
-            <p>{ props.subtext  && props.subtext}</p>
-            <div style={{position: 'relative', display: 'inline-block', width: 100+"%"}}>
-                <input type={props.type ? props.type === "password" ? passwordShown ? "text" : "password": "text" : props.type} value={props.value}  name={props.name}  placeholder={props.placeholder} style={{width: props.width +'px', marginBottom : 10, border: props.error ? "1px solid red": '', backgroundColor: props.error ? "#FFEBEB": ''  }} onChange = {props.onChange}/>
-                {props.name === "password" && <FontAwesomeIcon icon={passwordShown ? faEyeSlash : faEye} className="eye" size="lg" style={{position: 'absolute', right: 20, top: 20, width: 20, height: 20, color : '#8B8C8C'}} onClick={togglePasswordVisiblity}/>}
-            </div>
-           
-            <p  style={{color:'red', height:5, width:100+'%'}} > {props.error ? props.error : " "}</p>
-        </div>
-    )
+    </div>
+  )
 }
 export default Singleinput;
-=======
-      <p style={{ color: "red", height: 5, width: 100 + "%" }}>
-        {" "}
-        {props.error ? props.error : " "}
-      </p>
-    </div>
-  );
-};
-export default Singleinput;
->>>>>>> 8007ad382bd7a9f787874a8bad72b6ebfd55e456
